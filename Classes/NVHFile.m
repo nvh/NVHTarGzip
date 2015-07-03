ea860cb5e7559580f4ce29afb4b2467555d2c693
Niels van Hoorn <nvh@nvh.io>
2015-07-03 15:08:52 +0200

    Move NVHProgress to seperate file

--- Classes/NVHFile.m
+++ Classes/NVHFile.m
@@ -7,63 +7,8 @@
 //
 
 #import "NVHFile.h"
-/** Using a small maximum total unit count instead of using self.fileSize
- * directly is recommended to let it work nicely with parent progress objects
- * because of this bug rdar://16444353 (http://openradar.appspot.com/radar?id=5775860476936192)
- *
- * Default is 100;
- */
-const int64_t NVHProgressMaxTotalUnitCount = 100;
-
-
-@interface NVHProgress : NSObject
-
-- (void)setVirtualTotalUnitCount:(int64_t)virtualTotalUnitCount;
-- (void)setVirtualCompletedUnitCount:(int64_t)virtualUnitCount;
-
-@end
-
-
-@interface NVHProgress ()
-
-@property (nonatomic) NSProgress *progress;
-@property (nonatomic, assign) CGFloat countFraction;
-
-@end
-
-
-@implementation NVHProgress
-
-// Designated initializer;
-- (instancetype)init
-{
-    self = [super init];
-    if (!self) { return nil; }
-    
-    self.progress = [NSProgress progressWithTotalUnitCount:NVHProgressMaxTotalUnitCount];
-    self.progress.cancellable = NO;
-    self.progress.pausable = NO;
-    
-    return self;
-}
-
-- (void)setVirtualTotalUnitCount:(int64_t)virtualTotalUnitCount
-{
-    self.countFraction = NVHProgressMaxTotalUnitCount / (float)virtualTotalUnitCount;
-}
-
-- (void)setVirtualCompletedUnitCount:(int64_t)virtualUnitCount
-{
-    self.progress.completedUnitCount = roundf( self.countFraction * virtualUnitCount );
-}
-
-- (void)setVirtualCompletedUnitCountToTotal
-{
-    self.progress.completedUnitCount = NVHProgressMaxTotalUnitCount;
-}
-
-@end
 #import "NSFileManager+NVHFileSize.h"
+#import "NVHProgress.h"
 
 
 @interface NVHFile ()
